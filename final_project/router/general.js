40bbf0bc7b26dfe4bbbcddfe29610b0d3fcec50c
Syed Ammar Ali <ammar.ali.skipq@gmail.com>
2023-03-12 05:01:58 +0500

    Final Commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -88,94 +88,81 @@ public_users.get('/review/:isbn',function (req, res) {
 });
 
 // Task 10 
-// Add the code for getting the list of books available in the shop (done in Task 1) using Promise callbacks or async-await with Axios
-
-function getBookList(){
-  return new Promise((resolve,reject)=>{
-    resolve(books);
-  })
-}
-
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  getBookList().then(
-    (bk)=>res.send(JSON.stringify(bk, null, 4)),
-    (error) => res.send("denied")
-  );  
+
+public_users.get('/', async (req, res) => {
+  try {
+    const bk = await getBookList();
+    res.send(JSON.stringify(bk, null, 4));
+  } catch (error) {
+    res.send("denied");
+  }
 });
+
+
 
 // Task 11
-// Add the code for getting the book details based on ISBN (done in Task 2) using Promise callbacks or async-await with Axios.
-
-function getFromISBN(isbn){
-  let book_ = books[isbn];  
-  return new Promise((resolve,reject)=>{
-    if (book_) {
-      resolve(book_);
-    }else{
-      reject("Unable to find book!");
-    }    
-  })
-}
-
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
-  getFromISBN(isbn).then(
-    (bk)=>res.send(JSON.stringify(bk, null, 4)),
-    (error) => res.send(error)
-  )
- });
+public_users.get('/isbn/:isbn', async (req, res) => {
+  const myPromise1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const isbn = req.params.isbn;
+      resolve(JSON.stringify(books[isbn], null, 4));
+    }, 3000);
+  });
 
-// Task 12
-// Add the code for getting the book details based on Author (done in Task 3) using Promise callbacks or async-await with Axios.
-
-function getFromAuthor(author){
-  let output = [];
-  return new Promise((resolve,reject)=>{
-    for (var isbn in books) {
-      let book_ = books[isbn];
-      if (book_.author === author){
-        output.push(book_);
-      }
-    }
-    resolve(output);  
-  })
-}
-
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  const author = req.params.author;
-  getFromAuthor(author)
-  .then(
-    result =>res.send(JSON.stringify(result, null, 4))
-  );
+  try {
+    const result = await myPromise1;
+    res.send(result);
+  } catch (error) {
+    res.send("Error occurred while retrieving book details.");
+  }
 });
 
-// Task 13
-// Add the code for getting the book details based on Title (done in Task 4) using Promise callbacks or async-await with Axios.
 
+// Task 12
+// Get book details based on author
+public_users.get('/author/:author', async (req, res) => {
+  const author = req.params.author;
+  const size = Object.keys(books).length;
+  let book;
 
-function getFromTitle(title){
-  let output = [];
-  return new Promise((resolve,reject)=>{
-    for (var isbn in books) {
-      let book_ = books[isbn];
-      if (book_.title === title){
-        output.push(book_);
-      }
+  for (let i = 1; i <= size; i++) {
+    if (books[i].author == author) {
+      book = books[i];
+      break;
     }
-    resolve(output);  
-  })
-}
+  }
 
+  if (book) {
+    res.send(book);
+  } else {
+    res.send("Book not found");
+  }
+});
+
+
+
+
+// Task 13
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async (req, res) => {
   const title = req.params.title;
-  getFromTitle(title)
-  .then(
-    result =>res.send(JSON.stringify(result, null, 4))
-  );
+  const size = Object.keys(books).length;
+  let book;
+
+  for (let i = 1; i <= size; i++) {
+    if (books[i].title == title) {
+      book = books[i];
+      break;
+    }
+  }
+
+  if (book) {
+    res.send(book);
+  } else {
+    res.send("Book not found");
+  }
 });
 
 module.exports.general = public_users;
